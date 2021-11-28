3bdebc41e1829acc4dcfa6edc03d5a9efbfcf2c6
alex <74060189+alexredk@users.noreply.github.com>
2021-11-28 10:30:03 +0200

    aa
    aa

---
+++ 2.py
@@ -0,0 +1,6 @@
+n = int(input("Введите значение"))
+R = int(input("ВВедите значение"))
+x = 1
+for i in range(1,n+1):
+    y = i**2
+    print(y)
---
+++ 3.py
@@ -0,0 +1,5 @@
+n = int(input("Введите значение"))
+S = 1+2+3+4
+for x in range(1,n+1):
+    S+= x
+    print(S)
